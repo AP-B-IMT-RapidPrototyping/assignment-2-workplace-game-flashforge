aaf9d04e06ebb03a800c913faf35320582f186df
woutik3 <wouters.stef.hilde@gmail.com>
2026-03-17 00:37:34 +0100

    afwerking auto test

--- project/scripts/Stef/AutoWerking.cs
+++ project/scripts/Stef/AutoWerking.cs
@@ -3,20 +3,31 @@ using System;
 
 public partial class AutoWerking : Node
 {
-	[Export] public float MaxFuel = 100f;
+    [Export] public float MaxFuel = 100f;
     public float CurrentFuel;
-    [Export] public Marker3D MotorSlot;
 
     public override void _Ready()
     {
         CurrentFuel = MaxFuel;
     }
 
+    public void InteractieMetOnderdeel(Node3D collider, AutoOnderdeel nieuwOnderdeel)
+    {
+        if (collider.GetParent() is Marker3D slot)
+        {
+            GD.Print($"Onderdeel vervangen in slot: {slot.Name}");
+            EquipPart(slot, nieuwOnderdeel);
+        }
+    }
+
     public void EquipPart(Marker3D slot, AutoOnderdeel part)
     {
         foreach (var child in slot.GetChildren())
         {
-            child.QueueFree();
+            if (child is not StaticBody3D) 
+            {
+                child.QueueFree();
+            }
         }
         
         if (part?.Onderdeel != null)
